0c90bb70f928c0b4de9aec1fb1b6838282eff08d
Gary DeReese <garydereese@sbcglobal.net>
2021-05-24 18:47:50 -0500

    added index.js to component folder for imports

---
+++ src/components/index.js
@@ -0,0 +1,69 @@
+import Block from "./Block";
+import Box from "./Box";
+import Button from "./Button";
+import Buttons from "./Buttons";
+import Card from "./Card";
+import Container from "./Container";
+import Content from "./Content";
+import Control from "./Control";
+import Delete from "./Delete";
+import Dropdown from "./Dropdown";
+import Field from "./Field";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Icon from "./Icon";
+import Image from "./Image";
+import ImageContainer from "./ImageContainer";
+import Input from "./Input";
+import Level from "./Level";
+import LevelItem from "./LevelItem";
+import Menu from "./Menu";
+import Message from "./Message";
+import Modal from "./Modal";
+import ModalCard from "./ModalCard";
+import NavigationBar from "./NavigationBar";
+import Notification from "./Notification";
+import ProgressBar from "./ProgressBar";
+import Section from "./Section";
+import Subtitle from "./Subtitle";
+import Table from "./Table";
+import TableContainer from "./TableContainer";
+import Tag from "./Tag";
+import Tags from "./Tags";
+import Title from "./Title";
+
+export {
+    Block,
+    Box,
+    Button,
+    Buttons,
+    Card,
+    Container,
+    Content,
+    Control,
+    Delete,
+    Dropdown,
+    Field,
+    Footer,
+    Hero,
+    Icon,
+    Image,
+    ImageContainer,
+    Input,
+    Level,
+    LevelItem,
+    Menu,
+    Message,
+    Modal,
+    ModalCard,
+    NavigationBar,
+    Notification,
+    ProgressBar,
+    Section,
+    Subtitle,
+    Table,
+    TableContainer,
+    Tag,
+    Tags,
+    Title,
+}
