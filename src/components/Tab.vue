d6462d3f539cf0f356bcc9f569ac0eefcf51b5b4
Gary DeReese <garydereese@sbcglobal.net>
2021-06-09 08:11:53 -0500

    added Tabs and Tab components

---
+++ src/components/Tab.vue
@@ -0,0 +1,27 @@
+<template>
+  <li :class="cls">
+    <a>
+      <slot></slot>
+    </a>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "Tab",
+  props: {
+    isActive: Boolean
+  },
+  computed: {
+    cls() {
+      const cls = {
+        "is-active": this.isActive,
+      };
+
+      return cls;
+    },
+  },
+}
+</script>
+
+<style></style>
---
+++ src/components/Tabs.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="tabs" :class="cls">
+    <ul>
+      <slot></slot>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Tabs",
+  props: {
+    alignment: String,
+    size: String,
+    isBoxed: Boolean,
+    isFullwidth: Boolean,
+    isToggle: Boolean,
+    isToggleRounded: Boolean,
+  },
+  computed: {
+    cls() {
+      const cls = {
+        "is-boxed": this.isBoxed,
+        "is-fullwidth": this.isFullwidth,
+        "is-toggle": this.isToggle,
+        "is-toggle-rounded": this.isToggleRounded,
+      };
+
+      if (this.alignment) {
+        const alignmentClassName = `is-${this.alignment.toLowerCase()}`;
+        cls[alignmentClassName] = true;
+      }
+      if (this.size) {
+        const sizeClassName = `is-${this.size.toLowerCase()}`;
+        cls[sizeClassName] = true;
+      }
+
+      return cls;
+    },
+  },
+};
+</script>
+
+<style></style>
